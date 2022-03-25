2cadbe43550206f2311f1a69b5f05501d5dc4670
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-03-25 09:42:49 -0500

    x

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const app = require("./app");
-const logger = require("./utils/logger");
+// const logger = require("./utils/logger");
 
 const server = app.listen(app.get("port"), () => {
-  logger.info(`Server running in port ${app.get("port")}`);
+  console.log(`Server running in port ${app.get("port")}`);
+  // logger.info(`Server running in port ${app.get("port")}`);
 });
 
 module.exports = { server, app };
